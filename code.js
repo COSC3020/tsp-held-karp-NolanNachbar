cba785bdb6d67e1133f012fd6abdff5275c9360a
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-14 10:11:45 -0700

    Update code.js

--- code.js
+++ code.js
@@ -8,7 +8,36 @@
 //         // reduce the set of cities that are unvisited by one  (the old start), set the new start, add on the distance from old start to new start
 //         heldKarp(cities - start, city) + distance from start to city
 
-
 function tsp_hk(distance_matrix) {
-    return -1;
+    let n = distance_matrix.length;
+    let visited = Array(n).fill(0);
+    let unvisited = Array.from({ length: n }, (v, i) => i);
+    
+    function heldKarp(cities, start) {
+        // if |cities| == 2
+        if (cities.length === 2) {
+            return distance_matrix[cities[0]][cities[1]];
+        }
+
+        let min = Infinity;
+
+        for (let i = 0; i < cities.length; i++) {
+            let currentCity = cities[i];
+            
+            if (visited[currentCity] === 1 || currentCity === start) continue;
+
+            visited[currentCity] = 1;
+            
+            let remainingCities = cities.filter(city => city !== currentCity);
+            let cost = heldKarp(remainingCities, currentCity) + distance_matrix[start][currentCity];
+            
+            min = Math.min(min, cost);
+
+            visited[currentCity] = 0;
+        }
+        
+        return min;
+    }
+
+    return heldKarp(unvisited, 0);
 }
